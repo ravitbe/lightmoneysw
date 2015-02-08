b954d288f56e9b0921622ee568601e96bdd4419c
ravitbe <ravitbe@users.noreply.github.com>
2015-02-08 16:05:16 +0200

    Update controlService.py

--- server/controlService.py
+++ server/controlService.py
@@ -44,6 +44,14 @@ def control_service_thread():
     except KeyboardInterrupt:
         print '^C received, shutting down the web server'
         server.socket.close()
+        
+
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='path/to/localhost.pem', server_side=True)
+httpd.serve_forever()
 
 
 
